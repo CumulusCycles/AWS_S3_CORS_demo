c9673c467abb871a7c41d7024039951064165655
CumulusCycles <cumuluscycles@gmail.com>
2023-07-01 03:10:36 -0400

    init commit

---
+++ CORS_site/code/js/scripts.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function displayRep(rep, contactsRow) {
+  $(contactsRow).append(`
+                        <div class="col-lg-4"><h2 class="fw-normal">${rep.region}</h2>
+                          <p class="text-start">
+                            name: ${rep.fname} ${rep.lname}
+                            <br />phone: ${rep.phone}
+                            <br />email: <a href="mailto:${rep.email}">${rep.email}</a>
+                          </p>
+                          </div>
+                      `);
+}
+
+$(document).ready(() => {
+  let contactsRow = $('#contacts-row');
+
+  $.getJSON("https://cumulus-cycling-website-assets.s3.amazonaws.com/data/reps.json", reps => {
+    reps.forEach(rep => {
+      displayRep(rep, contactsRow);
+    });
+  });
+});
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AWS_S3_CORS_demo
-Code for the S3 Bucket with CORS video.
+Code for the S3 Bucket with CORS [video](https://www.youtube.com/watch?v=iHmCKHpxQlQ&list=PLRBkbp6t5gM0cJwCiPZzo1ahZ6M5wHHU1&index=2).
+
+Credit: Icons created by [flaticon.com](https://www.flaticon.com/)
